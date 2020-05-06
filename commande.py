9f4230c9cc94e9ecd9c0894ecdf3aadf1fb1a053
oscarg29 <63099432+oscarg29@users.noreply.github.com>
2020-05-06 11:38:20 +0200

    Multiple codes for different subjects

---
+++ commande.py
@@ -0,0 +1,13 @@
+#Donne tout les positions desire pour effectuer un trajectoire
+position=[  [0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 0, 0, 1]]
+
+#Donne tout les rotations desire pour chaque position
+rotation=[  [0, 0, 0],
+            [-57, 0, 0],
+            [0, 0, 0],
+            [-57, 0, 0],
+            [0, 0, 0]]
